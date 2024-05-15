9edbe2a394f791352ffc42d3a10934e99667d02b
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2024-05-15 20:54:55 +0200

    switch to syncoverycl like start to be able to end RS gracefully

--- scripts/dockerfile/files/start.sh
+++ scripts/dockerfile/files/start.sh
@@ -3,3 +3,32 @@
 echo "OS Date: $(date)"
 echo "Starting SyncoveryRS"
 /syncovery/SyncoveryRS
+
+
+# initialize trap to forceful stop the bot
+trap terminator SIGHUP SIGINT SIGQUIT SIGTERM
+function terminator() {
+  echo
+  echo "Stopping SyncoveryRS $child..."
+  kill -TERM "$child" 2>/dev/null
+  stop
+  echo "Exiting."
+}
+
+function stop() {
+  kill "$child"
+}
+
+function start() {
+  echo "OS Date: $(date)"
+  mkdir -p ${SYNCOVERY_HOME}/.Syncovery
+
+  echo "Starting RemoteService"
+  /syncovery/SyncoveryRS >> ${SYNCOVERY_HOME}/RS.log &
+}
+
+start
+tail -f ${SYNCOVERY_HOME}/RS.log &
+
+child=$!
+wait "$child"
